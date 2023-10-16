abc1b416469746af6392d6439326beeca662d990
yogesh3656 <97842120+yogesh3656@users.noreply.github.com>
2023-10-16 10:34:06 +0530

    Create mcqs1_a.js

---
+++ mcqs1_a.js
@@ -0,0 +1,259 @@
+    const questions =  [
+            {
+                question: "The .......... effects are used to apply movement to the images and objects in slides.",
+                options: ["(a).transition", "(b).animation", "(c).Both (a) and (b)", "(d). None of these"],
+                correctAnswer: 1
+            },
+            {
+                question: "The list of coded instructions is called",
+                options: ["computer program", "algorithm", "utility program", "None of these"],
+                correctAnswer: 0
+            },
+            {
+                question: "The time it takes a device to lacate data and instructions and make them available to the CPU is know as",
+                options: ["clock speed", "a processing cycle", "CPU speed", "access time"],
+                correctAnswer: 3
+            },
+
+          
+{
+    question: "Which output device is used for translating information from a computer into pictorial form on paper?",
+    options: ["Mouse", "Plotter", "Punched cards", "None of these"],
+    correctAnswer: 1
+},
+{
+    question: "A general purpose single user microcomputer designed to be operated by one person at a time is ",
+    options: ["special purpose computer ", "KIPS", "KB", "PC"],
+    correctAnswer: 3
+},
+{
+    question: "In which of the following tab, you can find proofing group in MS-Word?",
+    options: ["View", "Review", "Insert", "New"],
+    correctAnswer: 1
+},
+{
+    question: "IT stands for ",
+    options: ["Integrated Technology", "Interesting Technology", "Information Technology", "Intelligent Technology"],
+    correctAnswer: 2
+},
+{
+    question: "Which operations in not performed by computer?",
+    options: ["Inputting", "Processing", "Controlling", "Understanding"],
+    correctAnswer: 3
+},
+{
+    question: "When you copy a formula ",
+    options: ["Excel edits cell references in the newly copied formula ", "Excel adjusts absolute cell references", "Excel erases the original copy of the formula", "Excel doen't adjust relative cell references"],
+    correctAnswer: 0
+},
+{
+    question: "The symbol used in an assembly language are",
+    options: ["codes", "mnemonics", "assembler", "All of these"],
+    correctAnswer: 1
+},
+{
+    question: "CD-ROM stands for",
+    options: ["Compactable Read Only Memory", "Compact Data Read Only Memory", "Compactable Disk Read Only Memory", "Compact Disk Read Only Memory"],
+    correctAnswer: 3
+},
+{
+    question: "Operating system is the most common type of .......... software.",
+    options: ["management", "application", "system", "communication"],
+    correctAnswer: 2
+},
+{
+    question: "Which of the following is not an option of clipboard group in Home tab?",
+    options: ["Cut", "Format Painter", "Copy", "Page setup"],
+    correctAnswer: 3
+},
+{
+    question: "........... is a form of permanent memory that holds all the instructionsthe computer needs to start up does not get erased when the power is turn off.",
+    options: ["The Network Interface Card (NIC)", "The CPU", "RAM ", "ROM"],
+    correctAnswer: 3
+},
+{
+    question: "Which of the following is the popular name of the norms by which a bank satisfies itself about the customer's identity and activities?",
+    options: ["Basel norms", "KYC norms", "Service norms", "Lending norms"],
+    correctAnswer: 1
+},
+{
+    question: "A website address is an unique name that identifies a specific .............. on the Web.",
+    options: ["web browser", "pda", "website", "link"],
+    correctAnswer: 2
+},
+{
+    question: "Line spacing refers to ",
+    options: ["(a). the space between the lines of text", "(b). the height of lines", "(c). the length of line", "(d). Both (a) and (c)"],
+    correctAnswer: 0
+},
+{
+    question: "An Excel workbook is a collections of ",
+    options: ["workbooks", "charts", "worksheets", "worksheets and charts"],
+    correctAnswer: 3
+},
+{
+    question: "DVD stands for",
+    options: ["Digital Video Disc", "Digital and Video Drive", "Double Volume Density", "Disk Virtual Drive"],
+    correctAnswer: 0
+},
+{
+    question: "Binary means",
+    options: ["there are two possibilies: on and off", "the same as a byte: 8 bits", "there are three options: 0,1, and 2", "that computers really need to have three or more options"],
+    correctAnswer: 0
+},
+{
+    question: "To log on to the network, a user must enter their",
+    options: ["money", "username and password", "full name", "job profile"],
+    correctAnswer: 1
+},
+{
+    question: "Microsoft Excel is a powerful",
+    options: ["word processing software", "spreadsheet package", "DBMS package", "communication package"],
+    correctAnswer: 1
+},
+{
+    question: "UNIVAC is",
+    options: ["Universal Automatic Computer", "Unique Automatic Computer", "Universal Array Computer", "Unvalued Automatic Computer"],
+    correctAnswer: 0
+},
+{
+    question: "The hardware device commonly referred to as the brain of the computer is the ",
+    options: ["RAM chip", "data input", "CPU ", "secondary storage"],
+    correctAnswer: 2
+},
+{
+    question: "Which of the following is first generation computer?",
+    options: ["EDSAC", "CDC-1604", "1BM-1401", "None of these"],
+    correctAnswer: 0
+},
+{
+    question: "Websites contain many pages that can be navigated using ",
+    options: ["bars", "threads", "links", "strings"],
+    correctAnswer: 2
+},
+{
+    question: "Chief component of first generation computer was",
+    options: ["translators", "vaccum tubes", "integrated circuits", "None of these"],
+    correctAnswer: 1
+},
+{
+    question: "Which of the follwing is required to communicate between two computers?",
+    options: ["Communications software", "Protocol", "Communication hardware", "All of the above"],
+    correctAnswer: 3
+},
+{
+    question: "In the URL http://www.google.com, the portion labelled http is the ",
+    options: ["host", "domain name", "protocol", "top-level domain"],
+    correctAnswer: 2
+},
+{
+    question: "A dedicated computer ",
+    options: ["is used by one person only", "is assigned one and only one task", "uses only one kind of software", "is meant for application software"],
+    correctAnswer: 1
+},
+{
+    question: "The number of pixels displayed on a screen is",
+    options: ["resolution", "colur depth", "refresh rate", "viewing size"],
+    correctAnswer: 0
+},
+{
+    question: "Word processing, spreadsheet, and photo-editing are example of ",
+    options: ["application software", "system software", "operating system software", "platform software"],
+    correctAnswer: 0
+},
+{
+    question: "Accidently, you made a mistake when working with your document. How can you undo that action?",
+    options: ["Ctrl+X ", "Ctrl+Y", "Ctrl+Z", "Ctrl+U"],
+    correctAnswer: 2
+},
+{
+    question: "The ............. shows all the websites and pages that you have visited over a period of time.",
+    options: ["status bar", "task bar", "history list", "tool bar"],
+    correctAnswer: 2
+},
+{
+    question: "Which of the following is domain name suffixes?",
+    options: [".com", ".net", ".com and .net", "None of these"],
+    correctAnswer: 0
+},
+{
+    question: "Who is the father of computer?",
+    options: ["Charles Babbage", "Ada Augusta", "John Napier", "None of these"],
+    correctAnswer: 0
+},
+{
+    question: "Each excel file is called workbook because",
+    options: ["it can contain text and data.", "it can be modified", "it can contain many sheets worksheets and chart sheets", "None of the above"],
+    correctAnswer: 2
+},
+{
+    question: "FORTRAN is ",
+    options: ["File Transition", "Format Translation", "Formula Translation", "Floppy Translation"],
+    correctAnswer: 2
+},
+{
+    question: "An output device that uses words or message recorded on a magnetic medium to produce audio response is ",
+    options: ["magnetic tape", "voice response unit", "voice recognition unit", "voice band"],
+    correctAnswer: 1
+},
+{
+    question: "A gigabyte is equal to ",
+    options: ["1024 GB", "1024 MB", "1024*1024 MB", "1024 KB"],
+    correctAnswer: 1
+},
+{
+    question: "Which of the following is a form of data ",
+    options: ["numbers and characters ", "imamges", "sound", "All of these"],
+    correctAnswer: 3
+},
+{
+    question: "The term bit is short for ",
+    options: ["megabyte", "binary language", "binary digit", "binary number"],
+    correctAnswer: 2
+},
+{
+    question: "The PowerPoint view that displays only text (title and be bullet) is ",
+    options: ["slide show", "slide sorter view", "notes page view", "outline view"],
+    correctAnswer: 3
+},
+{
+    question: "RTGS means",
+    options: ["Real time genocide settlement", "Regional time genocide settlement", "Real time gross settlement", "Real time general settlement"],
+    correctAnswer: 2
+},
+{
+    question: "The modem does not",
+    options: ["produce sound similar to those you hear on the touch tone telephone.", "collect signals from low speed terminals and transmit them over a signal.", "line modulate electrical digital signal into an analog signal.", "demodulate an analog signal into digital signal."],
+    correctAnswer: 0
+},
+{
+    question: "A(n) ................ is private corporate network, used execlusively by company employees",
+    options: ["internet", "local area network", "peer-to-peer", "intranet"],
+    correctAnswer: 1
+},
+{
+    question: "The keystrokes Ctrl + I is used to ",
+    options: ["increase font size", "insert a line break", "indicate the text show be bold", "applies italic format to the selected text"],
+    correctAnswer: 3
+},
+{
+    question: "Which of the following is not a valid data type of excel?",
+    options: ["Number", "Character", "Label", "Date/time"],
+    correctAnswer: 1
+},
+{
+    question: "............ allows LAN users to share computer programs and data.",
+    options: ["Communication server", "Print server", "File server", "None of the above"],
+    correctAnswer: 2
+},
+{
+    question: "The e-commerce domain that involves business activity initiated by the consumer and targeted to businesses is known as ",
+    options: ["Consumer to Consumer (C2C)", "Business to Business (B2B)", "Consumer to Business (C2B)", "Business to Consumer (B2C)"],
+    correctAnswer: 2
+}
+          
+          
+          
+          
+      
+        ];
