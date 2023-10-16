51453d032eed27cbe01fc838cf75e5368b6bfea2
yogesh3656 <97842120+yogesh3656@users.noreply.github.com>
2023-10-16 15:04:21 +0530

    Create mcq2b.js

---
+++ mcq2b.js
@@ -0,0 +1,256 @@
+ const questionsB =  [
+{
+    question: "In the E-mail address Arihant@sou.edu, "Arihant" is passward.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "In digital computers, values are measured by comparison.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Refresh button is clicked to go to the another website.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "MICR technology is used by banks for faster processing of large volume of cheques.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Application software acts as an interface between operating system and hardware.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "VDU can be both input and output devices.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Worm is also known as virus.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Word processing software is used primarily with numbers to perform calculations and create graphs.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Mobile wallet is a credit or debit card.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "ROM stands for Random Only Memory",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Servers are computers that provides resources to other computer connected to a network.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Hardware is the set of computer instructions or programs that enables the software to perform different tasks.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Data can be a number, a word, a picture, or a sound.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Keyboard, mouse and scannerare output devices.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "A cell entry can be edited either in the cell or in the formula bar.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Scanner is input device used to captures videos.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "A series of specially designed documents, all linked together to be viewed on the Internet is known as World Wide Web.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Pixel is the number of dots per inch.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Most computer-related errors are the result of improperly entered data .",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "CAPS LOCK key helps you in typing letters in uppercase.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "The title bar displays the name of current active word document.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Hardware is any part of the computer that you can physically touch.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Multiprogramming involves the "simultaneous" operation of many jobs on the computer.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Artifical Intelligence is the field of study that enables computers to think like human beings.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Group of 8 bits is called a byte.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "You can open a file or folder by double-clicking.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Properly arranged data is called information.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Only one Excel Spreadsheet can be in an Excel Workbook.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Supercomputers are the fastest and most expensive computers.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Machine language is independent  of the type of microprocessor in a computer system.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "A hand written application is an example of a source document.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Mainframes are computers that are able to execute many different computer programs at same time.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "The data, which is available in the main memory is much faster then secondary memory.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "For sound recording speaker is necessary.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "The well known non-impact printers are inkjet laser printers.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Headers and footers are displayed only on first page in a document.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Memory management is a main function of CPU.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Airtel Money is a wallet.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "USB stands for Uniform Serial Bus.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Pen drive is a type of secondary memory data storage device.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "The data once stored in the CD-R discs cannot be erased.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "the set of programs necessary to carry out operations for a specified job is called hardware.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Star topology requires a multipoint connections.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Unitprice is a valid name for a cell or range.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Cathode Ray Tube (CRT) and Liquid Crystal Dislpay (LCD) are types of mouse available in the market.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "A icon on the desktop that represents a temporary holding place for files that are deleted is called as recycle bin.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "DVD-ROM is an optical disc that is capable of storing data from disc that is capable of storing data from 4.7 GB to 17GB.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "When you turn on background printing you can continue working in PowerPoint while you print.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Primary memory is also known as main memory of the computer.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Canara Bank is India's first Bank cerrified by ISO.",
+    options: ["True","False"],
+    correctAnswer: 0
+}
+
+      
+        ];
+
+export { questionsB };
