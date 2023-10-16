cc89e193cb2d5d3679da66e6315680365ea68a22
yogesh3656 <97842120+yogesh3656@users.noreply.github.com>
2023-10-16 12:00:43 +0530

    Create mcq1b.js

---
+++ mcq1b.js
@@ -0,0 +1,263 @@
+        // Define the questions and answers dynamically
+        const questionsB = [
+    
+{
+    question: "Computer work when instructions in any language are given to it.",
+    options: ["True", "False"],
+    correctAnswer: 1
+},
+{
+    question: "Tables can be drawn through insert tab in MS-Word.",
+    options: ["True", "False"],
+    correctAnswer: 0
+},
+{
+    question: "Tracing or Clearing cell content is same as deleting a cell.",
+    options: ["True ", "False"],
+    correctAnswer: 1
+},
+{
+    question: "WWW is a system of interlinked web hypertext documents, unavailable on Internet.",
+    options: ["True ", "False"],
+    correctAnswer: 1
+},
+{
+    question: "Computer program that helps to view WWW document is called Web browser.",
+    options: ["True", "False"],
+    correctAnswer: 0
+},
+{
+    question: "A font is a named set of characters that have the same characteristics.",
+    options: ["True ", "False"],
+    correctAnswer: 0
+},
+{
+    question: "We can't delete the Recycle bin icon but we can make it invisible.",
+    options: ["True ", "False"],
+    correctAnswer: 0
+},
+{
+    question: "Linux and MS Windows may be installed on one system.",
+    options: ["True ", "False"],
+    correctAnswer: 0
+},
+{
+    question: "Nibble is a collection of files.",
+    options: ["True ", "False"],
+    correctAnswer: 1
+},
+{
+    question: "There is no limit on the amount of bunds that could be transferred using RTGS.",
+    options: ["True ", "False"],
+    correctAnswer: 1
+},
+{
+    question: "Rows are vertical lines in table.",
+    options: ["True ", "False"],
+    correctAnswer: 1
+},
+{
+    question: "The bits in byte are normally numbered from zero to six.",
+    options: ["True", "False"],
+    correctAnswer: 1
+},
+{
+    question: "Screensaver on your desktop appears when computer is idle for a while.",
+    options: ["True", "False"],
+    correctAnswer: 0
+},
+{
+    question: "WWW stands for World Wide Web.",
+    options: ["True", "False"],
+    correctAnswer: 0
+},
+{
+    question: "OMR is used for correction of MCQ paper.",
+    options: ["True ", "False"],
+    correctAnswer: 0
+},
+{
+    question: "KB stands for kit byte.",
+    options: ["True", "False"],
+    correctAnswer: 1
+},
+{
+    question: "There is only one wayto start Microsoft Word, which is by office shortcut bar.",
+    options: ["True", "False"],
+    correctAnswer: 1
+},
+{
+    question: "'Add/remove tools' is found in accessories.",
+    options: ["True", "False"],
+    correctAnswer: 1
+},
+{
+    question: "Super computer Super computer is a symbol of fifth generation computer.",
+    options: ["True ", "False"],
+    correctAnswer: 0
+},
+{
+    question: "Windows is the operating system of computer.",
+    options: ["True", "False"],
+    correctAnswer: 0
+},
+{
+    question: "In star topology, any node can communicate with any other node directly.",
+    options: ["True ", "False"],
+    correctAnswer: 1
+},
+{
+    question: "One of the advantages of a PC network is that the access to the Internet is quicker.",
+    options: ["True ", "False"],
+    correctAnswer: 1
+},
+{
+    question: "You can apply sound effects to your slides in your presentation.",
+    options: ["True", "False"],
+    correctAnswer: 0
+},
+{
+    question: "The small pictures you see on the desktop are called Task bar.",
+    options: ["True", "False"],
+    correctAnswer: 1
+},
+{
+    question: "Motherboard is also known as system board.",
+    options: ["True", "False"],
+    correctAnswer: 0
+},
+{
+    question: "RTOS i stands for Real Time Operating System.",
+    options: ["True ", "False"],
+    correctAnswer: 0
+},
+{
+    question: "Computer cannot understand the binary language.",
+    options: ["True", "False"],
+    correctAnswer: 1
+},
+{
+    question: "RAM is a temporary memory.",
+    options: ["True", "False"],
+    correctAnswer: 0
+},
+{
+    question: "Audio output devices can output only music.",
+    options: ["True", "False"],
+    correctAnswer: 1
+},
+{
+    question: "You can add items to the start menu of windows.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "MS-Power Point is a spread sheet software.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Computer is a manual machine.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Debugging is the process of finding errors in software code.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Computer is an electronic device.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "File structure in Linux is hierarchical.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "LLL is advance computer language.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "Softcopy is a term applied to multiple pages of computer-printed reports.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "By default the hyperlink color is blue.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "A cache memory is faster than main memory.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "1TB = 1024 MB",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "DBMS stands for DataBase Management System.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Point to point connection, more than two devices can share a single link.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Full form of 'e-commerce' is Electronic Commerce.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Multimedia software helps you carry out tasks, such as typing a document or creating a spreadsheet.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "You can cut or copy and paste a table to an other location in a document, just as you would text.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Payroll is a type of application software.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "The type of operating system software you use depends on your computer's application.",
+    options: ["True","False"],
+    correctAnswer: 1
+},
+{
+    question: "RAM is a volatile memory.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Word processing software is used to create letters, papers, or other basic documents.",
+    options: ["True","False"],
+    correctAnswer: 0
+},
+{
+    question: "Bank Mitra is a Business correpondet",
+    options: ["True","False"],
+    correctAnswer: 0
+}
+          
+      
+      
+                
+          
+          
+      
+        ];
+
+export { questionsB };
