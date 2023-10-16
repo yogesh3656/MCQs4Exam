34fa2f6fb4fb0b6f8162b0bcad9369ef5560a85d
yogesh3656 <97842120+yogesh3656@users.noreply.github.com>
2023-10-16 15:02:48 +0530

    Create mcq2a.js

---
+++ mcq2a.js
@@ -0,0 +1,256 @@
+ const questionsA =  [
+{
+    question: ".......... are devices used to transmit data over telecommunications lines.",
+    options: ["Drives", "Drives bays", "Modems", "Platforms"],
+    correctAnswer: 2
+},
+{
+    question: "An error is also known as",
+    options: ["debug", "bug", "cursor", "None of these"],
+    correctAnswer: 1
+},
+{
+    question: "What difference does the 5th generation computers have from other generation computer?",
+    options: ["Use of artificial Language", "Use of oop concepts", "both 'a' and 'b'", "None of the above"],
+    correctAnswer: 0
+},
+{
+    question: "In Microsoft Word to cut something the following shortcut is used",
+    options: ["Ctrl + C", "Ctrl + X", "Ctrl + V", "None of these"],
+    correctAnswer: 1
+},
+{
+    question: "Which is not common feature of software application?",
+    options: ["Menus", "Windows", "Help", "Search"],
+    correctAnswer: 1
+},
+{
+    question: "Dot-matrix, Deskjet, Inkjet and Laser are all type of which computer peripherals?",
+    options: ["Keyboards", "Software", "Monitors", "Printers"],
+    correctAnswer: 3
+},
+{
+    question: "NEFT means ",
+    options: ["National Electronic Funds Transfer System", "Negotiated Efficient Fund Transfer System", "National Efficient Fund Transfer Solution", "Non-Effective Fund Transfer System"],
+    correctAnswer: 0
+},
+{
+    question: "Personal computers can be connected together to form a ",
+    options: ["server", "super computer", "enterprise", "Network"],
+    correctAnswer: 3
+},
+{
+    question: "A word in a web page that, when clicked opens another document is called",
+    options: ["anchor", "hyperlink", "reference", "URL"],
+    correctAnswer: 1
+},
+{
+    question: "The Delete key delete the text:",
+    options: ["To the right of insertion point", "To the left of insertion point", "To the both sides of insertion point", "None of the above"],
+    correctAnswer: 0
+},
+{
+    question: "Flash drive is a type of ",
+    options: ["ROM ", "software", "hardware", "memory"],
+    correctAnswer: 3
+},
+{
+    question: "A dot or symbol placed at the front of some text to highlight, it is known",
+    options: ["drop cap", "bullets", "symbol", "None of these"],
+    correctAnswer: 1
+},
+{
+    question: "PROM in computer system stands for",
+    options: ["Programmable Read Only Memory", "Pulse Read Only Memory", "Packed Read Only Memory", "Parallel Read Only Memory"],
+    correctAnswer: 0
+},
+{
+    question: "Data gathering in computer means, they allow to use .......... data.",
+    options: ["present", "input", "output", "store"],
+    correctAnswer: 3
+},
+{
+    question: "Typical acronym of reusable optical storage will be",
+    options: ["CD", "CD-RW", "DVD", "RPM"],
+    correctAnswer: 1
+},
+{
+    question: "Storage which stores or retains data after power off is called",
+    options: ["volatile storage", "non-volatile storage", "sequential storage ", "direct storage"],
+    correctAnswer: 1
+},
+{
+    question: "You would use ........ software to create spreadsheets, type documents and edit photos.",
+    options: ["application", "utility", "system", "operating"],
+    correctAnswer: 0
+},
+{
+    question: "The term operating system means",
+    options: ["(a).A set of program which controls computer working", "(b).It is interface between software and hardware", "(c).Both (a) and (b)", "(d). It is programming language"],
+    correctAnswer: 2
+},
+{
+    question: "Most websites have a main page, the ................ which acts as a doorway to the rest of the website pages.",
+    options: ["search engine", "home page", "browser", "URL"],
+    correctAnswer: 1
+},
+{
+    question: ".......... is a collection of many separate networks.",
+    options: ["A WAN", "An Internet", "A LAN ", "None of these"],
+    correctAnswer: 3
+},
+{
+    question: "Name the first general purpose electronic computer.",
+    options: ["ADVAC", "ADSAC", "ENIAC", "EDVAC"],
+    correctAnswer: 2
+},
+{
+    question: "Which of the following is not one of the four major data processing functions of a computer?",
+    options: ["Gathering data", "Processing data into information", "Analysing the data or information", "Storing the data and information"],
+    correctAnswer: 2
+},
+{
+    question: "Which of the following is not a transmission medium?",
+    options: ["Modem", "Coaxial cables", "Telephone lines", "Microwave systems"],
+    correctAnswer: 0
+},
+{
+    question: "A .............. is a pre-designed format of text and color scheme.",
+    options: ["template", "slide", "presentation", "None of these"],
+    correctAnswer: 0
+},
+{
+    question: "A computer program that converts entire program into machine language in a single run is called a/an",
+    options: ["interpreter", "simulator", "compiler", "commander"],
+    correctAnswer: 2
+},
+{
+    question: "In order to play and hear sound on a computer one needs?",
+    options: ["Sound card and speaker", "A microphone ", "Both (a) and (b)", "None of the above"],
+    correctAnswer: 0
+},
+{
+    question: "The software that allows users to surf the Internet is called a/an ......... .",
+    options: ["search engine", "Internet Service Provider (ISP)", "Internet Surfing Provider", "web browser"],
+    correctAnswer: 3
+},
+{
+    question: "In PowerPoint, the following is not a feature of PowerPoint.",
+    options: ["Adding animations to the slides", "Printing the speaker's notes along with the slide images.", "Linking a slide transition with a laser pointer", "Printing more than one slide on a single sheet"],
+    correctAnswer: 2
+},
+{
+    question: "Storage capacity of magnetic disk depends on",
+    options: ["tracks per inch of surface", "bits per inch of tracks", "disk pack in disk surface", "All of these"],
+    correctAnswer: 3
+},
+{
+    question: "This was the first network.",
+    options: ["CSNET", "NSFNET", "ANSNET", "ARPANET"],
+    correctAnswer: 3
+},
+{
+    question: "A .......... is the term used when a search engine returns a web page that matches the search criteria.",
+    options: ["blog", "hit", "link", "None of these"],
+    correctAnswer: 2
+},
+{
+    question: "Which of the following is an advantage of using fiber optics data transmission?",
+    options: ["Resistance to data theft", "Fast data transmission rate", "Low noise level", "All of the above"],
+    correctAnswer: 3
+},
+{
+    question: "Which of the following looses its contents when the computer is turned off?",
+    options: ["RAM", "ROM ", "PROM", "All of the above"],
+    correctAnswer: 0
+},
+{
+    question: "To close a presentation, you have to press ",
+    options: ["Ctrl + U", "Ctrl + Shift", "Ctrl + H", "Ctrl + W"],
+    correctAnswer: 3
+},
+{
+    question: "HTML stands for ",
+    options: ["Hypertext Marking Links", "Hypertext markup language", "Higher textual Markig of Links", "Hypertext Mixer of Links"],
+    correctAnswer: 1
+},
+{
+    question: "What is a shell?",
+    options: ["It is a hardware componet", "It is a command interpreter", "It is a part in computer", "It is a tool in CPU scheduling "],
+    correctAnswer: 1
+},
+{
+    question: "A computer program that translates one program instructions at a time into a machine language is called a/an",
+    options: ["interpreter", "CPU", "compiler", "None of these"],
+    correctAnswer: 0
+},
+{
+    question: "The instructions for starting the computer are stored in",
+    options: ["random access memory", "CD-ROM", "read only memory chip", "All of the above"],
+    correctAnswer: 2
+},
+{
+    question: "Which is not the search engine?",
+    options: ["altavista.com", "google.com", "facebook.com", "yahoo.com"],
+    correctAnswer: 2
+},
+{
+    question: "The term user interface refers to ",
+    options: ["What the user sees on the screen and how they can interact with it.", "how the operating system responds to he user commands.", "the means by which the user interacts with the peripheral devices on the computer.", "None of the above."],
+    correctAnswer: 0
+},
+{
+    question: "What is the full form of CD-R?",
+    options: ["Collect Data-Readable", "Compact Disc-Recordable", "Cover Drink", "Carbon Data"],
+    correctAnswer: 1
+},
+{
+    question: "A mouse, trackball and joystick are the examples of ",
+    options: ["pointing devices", "pen input devices", "output devices", "None of these"],
+    correctAnswer: 0
+},
+{
+    question: "The utility which is used to increase the speed of programs is called",
+    options: ["Disk Defragmenter", "Disk Cleanup", "Disk Formatter", "None of the above"],
+    correctAnswer: 0
+},
+{
+    question: "Which of the following is otp?",
+    options: ["stable", "fixed", "dynamic", "static"],
+    correctAnswer: 2
+},
+{
+    question: "CU is a part of ",
+    options: ["CPU", "RAM", "ROM", "None of these"],
+    correctAnswer: 0
+},
+{
+    question: "CPU reads the information from secondary memory.",
+    options: ["through cache", "first, information is transferred to main memory and from there, the CPU reads", "through DMA", "None of the above"],
+    correctAnswer: 1
+},
+{
+    question: "You can use the Tab key to ",
+    options: ["move a cursor across the screen", "Indent a paragraph", "move the cursor down the screen", "Only (1) and (2)"],
+    correctAnswer: 1
+},
+{
+    question: "............. is done when the computer is turned on and the operating system is loading.",
+    options: ["Booting", "Taping ", "Flashing ", "Tracking"],
+    correctAnswer: 0
+},
+{
+    question: "Which of the following is known as plastic Money? <br> I. Demand Draft <br> II. Credit Card <br> III.Debit Card",
+    options: ["Only I", "Only II", "Only III", "Both II and III"],
+    correctAnswer: 3
+},
+{
+    question: "What is full form of OMR?",
+    options: ["Optical Mark Reader", "Optical Marked Reading", "Optical Moon Right", "Optical Me Right"],
+    correctAnswer: 0
+}
+
+         
+        ];
+
+export { questionsA };
